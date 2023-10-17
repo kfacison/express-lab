0f37c395ffe2f8bd3e3576ea3bfb519f1db25979
kfac <kfacison1998@gmail.com>
2023-10-16 19:30:12 -0500

    added ejs

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require("express");
+const path = require("path")
 //const spooky = require("./info")
 
 const page = express();
 //const scary = spooky();
 
+page.set("view engine","ejs");
+page.set("views",path.join(__dirname,),"views")
+
 function home(req, res){
-    res.send('<h1>Hello World!</h1>');
+    res.send("<h1>Hello World!</h1>");
 }
 
 function portMsg(){
-    console.log('Listening on port 3000');
+    console.log("Listening on port 3000");
 }
 
 function spook(req, res){
-    res.send('<h1>sPoOkY mOnTh</h1>');
+    res.send("<h1>sPoOkY mOnTh</h1>");
+}
+function renderHome(req, res){
+    res.render("./views/home");
 }
 
 page.get("/",home);
 page.get("/spook",spook);
+page.get("/home",renderHome);
 
 page.listen(3000, portMsg);
